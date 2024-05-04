563c0a72809ffea267bd328e1e2e9f46044e4721
elisabete <beta_viralhadas@hotmail.com>
2024-05-04 22:13:52 +0200

    collapse and center modal in mobile version

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -9,18 +9,14 @@ const Collapse = ({ title, description }) => {
 
   return (
     <div className="collapse">
-      <div className="collapse-header" onClick={toggleCollapse}>
+      <div className="collapse-header collapse_desktop" onClick={toggleCollapse}>
         <span style={{ marginTop: isOpen ? '165px' : '0' }}>{title}</span>
         <span style={{ display: isOpen ? 'none' : 'block' }} className={` ${isOpen ? 'open' : 'closed'}`}><i className="fa-solid fa-chevron-up arrow"></i></span>
-
-        
       </div>
-      {window.innerWidth <= 767 && isOpen && (
-          <div className='collapse_header_mobile' onClick={toggleCollapse}>
-            <span style={{ marginTop: isOpen ? '0' : '0' }}>{title}</span>
-            <span style={{ display: isOpen ? 'block' : 'block' }} className={` ${isOpen ? 'open' : 'closed'}`}><i className="fa-solid fa-chevron-up arrow"></i></span>
-          </div>
-        )}
+      <div className='collapse_header collapse_mobile' onClick={toggleCollapse}>
+        <span style={{ marginTop: isOpen ? '0' : '0' }}>{title}</span>
+        <span style={{ display: isOpen ? 'block' : 'block' }} className={` ${isOpen ? 'open' : 'closed'}`}><i className="fa-solid fa-chevron-up arrow"></i></span>
+      </div>
       {isOpen && <div className="collapse-content">{description}</div>}
     </div>
   );
